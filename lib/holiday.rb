6cb83548b0d9a770f80585c9766f7163508b0174
Nash Brancic <nashfft@gmail.com>
2019-02-08 04:38:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,15 +38,14 @@ end
   
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}"
-    holiday.each do |holiday, item|
-      puts "#{holiday.to_s.split("_").map{|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-    end
-  end
-end
     
-    
-    
+  
+  
+  
+  
+  
+  
+  
   
   
   
