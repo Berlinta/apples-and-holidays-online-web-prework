c5fd7993caa4247cd695c12c1c9f5065cc5e3fe6
Nash Brancic <nashfft@gmail.com>
2019-02-08 04:52:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,8 +46,6 @@ def all_supplies(holiday_supplies)
   end
 end
 
-all_supplies(holiday_supplies)
-      
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
